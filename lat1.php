4b81af890ae283d4540781c69208bc7c12446e9a
iwandwipras <iwan.prasetyo@students.amikom.ac.id>
2018-10-26 10:21:02 +0700

    Versi 1
    Coba

---
+++ lat1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+	$PWL=["Toni"=>80, "Dewi"=>90, "Nina"=>75, "Reza"=>60];
+	$AI=["Toni"=>80, "Dewi"=>90, "Nina"=>75, "Reza"=>60];
+	$SBD=["Toni"=>80, "Dewi"=>90, "Nina"=>75, "Reza"=>60];
+	echo "$PWL"["Toni"]["Dewi"]["Nina"]["Reza"];
+?>
---
+++ lat2.php
@@ -0,0 +1,11 @@
+<?php
+	$nilai = ["PWL", "AI", "SBD"];
+	foreach ($nilai as $key => $value) {
+
+		$nilaiPWL=array(
+
+			ton)
+		echo $value;
+		echo "<br>";
+	}
+?>
